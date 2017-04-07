e5942485ad94a4c8590466d4b3207146987d9688
Zeeshan Qureshi <zee@zqureshi.in>
2017-04-07 18:47:54 -0400

    Implement EchoClient

---
+++ echoserver/src/main/java/in/zqureshi/netty/EchoClient.java
@@ -0,0 +1,43 @@
+package in.zqureshi.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoClient {
+    private final String HOST = "localhost";
+    private final int PORT = 8022;
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                .channel(NioSocketChannel.class)
+                .remoteAddress(new InetSocketAddress(HOST, PORT))
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new EchoClientHandler());
+                    }
+                });
+
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting EchoClient...");
+        new EchoClient().start();
+    }
+}
